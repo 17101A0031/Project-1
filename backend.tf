4d60606ca0e809ea8520629e78c2fa0bfdb28b5b
Siddhesh Kanse <42892993+17101A0031@users.noreply.github.com>
2026-04-15 22:05:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "siddhesh-bucket-project-1"
+    bucket = "siddhesh-proj-1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
