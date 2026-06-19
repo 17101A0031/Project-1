152622ff32544de08bace662e1cae188e9896c30
Siddhesh Kanse <42892993+17101A0031@users.noreply.github.com>
2026-06-20 00:18:15 +0530

    Fix missing closing quote for region in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "siddhesh-project-1"
     key = "main"
-    region = "ap-south-1
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
